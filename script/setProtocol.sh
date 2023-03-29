c7a2863d313bde3d28321a8976ded5c23a6d0369
alikonuk1 <alikonuk1@gmail.com>
2023-03-29 17:49:53 +0300

    ✨ arrange scripts

--- script/setProtocol.sh
+++
@@ -1,9 +0,0 @@
-source .env
-
-export CONTRACT=0xf8d59ea68cb19d16bd66300cb49955479e45f879
-export ARG=0x2B68407d77B044237aE7f99369AA0347Ca44B129
-export SHARDEUM=https://liberty20.shardeum.org/
-
-#cast send --private-key=$LOCAL_PRIVATE_KEY $FACTORYERC20SB "setMintPrice(uint256)" $MINT_PRICE --rpc-url $LOCAL_RPC_URL --legacy
-
-cast send --private-key=$PRIVATE_KEY $CONTRACT "setProtocol(address)" $ARG --rpc-url $GOERLI_RPC_URL
--- script/verify.sh
+++ script/verify.sh
@@ -2,4 +2,4 @@ source .env
 
 forge verify-contract --chain-id 5 \ 
     --compiler-version v0.8.19+commit.7dd6d404 \ 
-    0xf8d59ea68cb19d16bd66300cb49955479e45f879 src/Pxswap.sol:Pxswap $ETHERSCAN_API_KEY 
+    0xf8d59ea68cb19d16bd66300cb49955479e45f879 src/Px.sol:Px $ETHERSCAN_API_KEY 
