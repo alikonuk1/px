ec13384f8e620f815585e7b8e7c35fe0d2c684ef
alikonuk1 <alikonuk1@gmail.com>
2023-03-31 17:37:34 +0300

    ✏️ update deployment addresses

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.18;
 
 import "forge-std/Script.sol";
 import {Px} from "src/Px.sol";
+import {Treasury} from "src/Treasury.sol";
 import {MockERC20} from "test/mock/MockERC20.sol";
 import {MockUni} from "test/mock/MockUni.sol";
 
@@ -11,14 +12,23 @@ contract Deploy is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        /*         MockERC20 usdc = new MockERC20( "Usdc", "USDC", 6);
-        MockERC20 weth = new MockERC20( "Weth", "WETH", 18); */
+        MockERC20 usdc = new MockERC20( "Usdc", "USDC", 6);
+        MockERC20 weth = new MockERC20( "Weth", "WETH", 18);
+
+        MockUni uni = new MockUni( 
+            0x94C10721Bc55E81E40E5Db92060335374F32546b, 
+            address(usdc), 
+            address(weth));
+
+        Treasury treasury = new Treasury();
 
         Px px = new Px(
             0x94C10721Bc55E81E40E5Db92060335374F32546b, 
-            0xe32BfD288d21Eea6022a27E40aADcE0A9612Dc10, 
-            0x80766F7635665e7100c527E716768A53ABda51ba, 
-            0xeA56A438A3098d5bBEF3Bf6F4bfaE6a754c26661);
+            address(usdc), 
+            address(weth),
+            address(treasury));
+
+        treasury.setPx(address(px));
 
         vm.stopBroadcast();
     }
--- script/deploy.sh
+++ script/deploy.sh
@@ -3,6 +3,6 @@ source .env
 #ETHERSCAN = $ETHERSCAN_API_KEY
 #POLYGONSCAN = $PSCAN_API_KEY
 
-forge script script/Deploy.s.sol:Deploy --chain-id 5 --rpc-url https://rpc.ankr.com/eth_goerli \
+forge script script/Deploy.s.sol:Deploy --chain-id 5 --rpc-url https://rpc.ankr.com/eth_sepolia \
     --broadcast --etherscan-api-key $ETHERSCAN_API_KEY \
     --verify -vvvv 
